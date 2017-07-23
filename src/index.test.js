1a71db877f0fa066c9ba73a115a51493f1a0c897
Sarath Chandra Achyuta Venkata <sachyutavenkata@innominds.com>
2017-07-23 05:42:31 +0530

    fix test scenario

--- src/index.test.js
+++ src/index.test.js
@@ -12,7 +12,6 @@ describe('index.html',() => {
   it('should say hello',(done) => {
     const index = fs.readFileSync('./src/index.html','utf-8');
     jsdom.env(index, function(err,window){
-      expect(true).to.equal(false);
       const content = window.document.getElementsByTagName('h1')[0];
       expect(content.innerHTML).to.equal('Hello NodeJS!');
       done();
